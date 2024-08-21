0d68f2fa512c7805f89c6824d9281e3bca994c7f
Chuner202 <70888456+Chuner202@users.noreply.github.com>
2024-08-21 13:03:15 +0300

    Create ecnr.cc
    Encryption/decryption for models.

---
+++ src/ecnr.cc
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+#ifdef WINVER
+void encryptDecrypt(const wchar_t* toEncrypt, int strLength, const wchar_t* key, wchar_t* output)
+{
+        int keyLength = wcslen(key);
+        for (int i = 0; i < strLength; ++i)
+        {
+                output[i] = toEncrypt[i] ^ key[i % keyLength];
+        }
+}
+#else
+void encryptDecrypt(const char* toEncrypt, int strLength, const char* key, char* output)
+{
+        int keyLength = strlen(key);
+        for (int i = 0; i < strLength; ++i)
+        {
+                output[i] = toEncrypt[i] ^ key[i % keyLength];
+        }
+}
+#endif
